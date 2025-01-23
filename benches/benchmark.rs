f657c2d0c4f1420e8169d94725c844b6781c792d
VictorGerin <gerinlacerda@gmail.com>
2025-01-22 23:09:06 -0300

    first beanch

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,20 +1,85 @@
-use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use std::time::Duration;
 
-fn fibonacci(n: u64) -> u64 {
-    match n {
-        0 => 1,
-        1 => 1,
-        n => fibonacci(n-1) + fibonacci(n-2),
+use criterion::{criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion, PlotConfiguration};
+use rand::{rngs::StdRng, SeedableRng, RngCore};
+use time_priority_order_algoritmh::*;
+
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+struct Obj {
+    start: i32,
+    end: i32,
+    priority: i32,
+}
+
+impl Timed<i32> for Obj {
+    fn get_start(&self) -> i32 {
+        self.start
+    }
+    fn get_end(&self) -> i32 {
+        self.end
+    }
+    fn set_start(&mut self, time: i32) {
+        self.start = time;
+    }
+    fn set_end(&mut self, time: i32) {
+        self.end = time;
     }
 }
 
+impl PartialOrd for Obj {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.priority.cmp(&other.priority))
+    }
+}
+
+fn create_test_data(size: &usize) -> Vec<Obj> {
+    let mut rng = StdRng::seed_from_u64(0);
+    
+    let mut data = Vec::with_capacity(*size);
+    for _ in 0..*size {
+        data.push(Obj {
+            start: rng.next_u32() as i32,
+            end: rng.next_u32() as i32,
+            priority: rng.next_u32() as i32,
+        });
+    }
+    data
+}
+
+const DEFAULT_TEST_SIZES: [usize; 5] = [
+    10_usize,
+    100_usize,
+    1_000_usize,
+    10_000_usize,
+    100_000_usize
+];
+
+
 fn criterion_benchmark(c: &mut Criterion) {
 
-    c.bench_function("fib 20", |b| {
-        b.iter(|| {
-            fibonacci(black_box(20))
-        })
-    });
+    let mut group = c.benchmark_group("heavy workload");
+    
+    group.plot_config(PlotConfiguration::default().summary_scale(AxisScale::Logarithmic));
+
+    group.measurement_time(Duration::new(30, 0));
+
+    for test_size in DEFAULT_TEST_SIZES.iter() {
+        group.bench_with_input(
+            BenchmarkId::new("time_order_by_priority", test_size),
+            test_size,
+            |b, size| {
+                b.iter_batched(
+                    || {
+                        create_test_data(size)
+                    },
+                    |vec| {
+                        time_order_by_priority(vec);
+                    }, criterion::BatchSize::SmallInput);
+            },
+        );
+    }
+    group.finish();
 
 }
 
---
+++ benchmark_results/v0.1.2/result.md
@@ -0,0 +1,5 @@
+
+
+## Heavy load ordenation
+**Description**: Given a random generated list of size n the time to sort it
+![img](./plot/random_objects.svg)
